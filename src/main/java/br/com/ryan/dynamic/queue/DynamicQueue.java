8af6bcb4360aa7ecdf72555d976f1b752436d877
= <ryaninternet62@gmail.com>
2022-09-25 10:26:07 -0300

    Complete methods to dynamic queue

--- src/main/java/br/com/ryan/dynamic/queue/DynamicQueue.java
+++ src/main/java/br/com/ryan/dynamic/queue/DynamicQueue.java
@@ -5,26 +5,66 @@ package br.com.ryan.dynamic.queue;
  */
 public class DynamicQueue<E> implements DynamicQueueInterface<E> {
 	
-	private Node<E> previous = null;
-	private Node<E> next = null;
+	private Node<E> end = null;
+	private Node<E> start = null;
 	
 	@Override
 	public boolean isEmpty() {
-		return (previous == null && next == null);
+		return (end == null && start == null);
 	}
 	
 	@Override
-	public void insert(Object e) {
+	public void insert(E e) {
+		Node<E> element = new Node<E>();
+		element.data = e;
 		
+		if (start == null) {
+			end = element;
+			start = element;
+			element.data = null;
+		} else {
+			if (start == end) {
+				end = element;
+				start.next = end;
+				end.next = null;
+			} else {
+				end.next = element;
+				element.data = null;
+				end = element;
+			}
+		}
 	}
 	
 	@Override
-	public E remove() {
-		return null;
+	public E remove() throws IndexOutOfBoundsException {
+		if (isEmpty()) {
+			throw new IndexOutOfBoundsException("Position do not exist");
+		}
+		
+		Node<E> temporary = start;
+		if (start != null && start == end) {
+			start = null;
+			end = null;
+		} else {
+			start = start.next;
+		}
+		
+		return temporary.data;
 	}
 	
 	@Override
 	public int size() {
-		return 0;
+		if (isEmpty()) {
+			return 0;
+		}
+		
+		int count = 0;
+		Node<E> temporary = start;
+		while (temporary != null) {
+			count++;
+			temporary = temporary.next;
+		}
+		
+		return count;
 	}
 }
--- src/test/java/br/com/ryan/dynamic/queue/DynamicQueueTest.java
+++ src/test/java/br/com/ryan/dynamic/queue/DynamicQueueTest.java
@@ -2,11 +2,12 @@ package br.com.ryan.dynamic.queue;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -14,62 +15,49 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 @TestInstance(Lifecycle.PER_CLASS)
 class DynamicQueueTest {
 	
-	private DynamicQueueInterface<Object> filaDinamica;
+	private DynamicQueueInterface<Object> dynamicQueue;
 	
 	@BeforeEach
 	void newInstance() {
-		filaDinamica = new DynamicQueue<Object>();
+		dynamicQueue = new DynamicQueue<Object>();
 	}
 	
 	@Test
+	@DisplayName("Empty true in new queue")
+	@Order(value = 0)
 	void emptyNew() {
-		assertTrue(filaDinamica.isEmpty());
+		assertTrue(dynamicQueue.isEmpty());
 	}
 	
 	@Test
+	@DisplayName("Empty false in add item queue")
+	@Order(value = 1)
 	void emptyWithAddItem() {
-		filaDinamica.insert("A");
-		assertFalse(filaDinamica.isEmpty());
+		dynamicQueue.insert("A");
+		assertFalse(dynamicQueue.isEmpty());
 	}
 	
 	@Test
+	@DisplayName("Empty true with add and remove order method")
+	@Order(value = 2)
 	void emptyReset() {
-		filaDinamica.insert("A");
-		filaDinamica.remove();
+		dynamicQueue.insert("A");
+		dynamicQueue.remove();
 		
-		assertTrue(filaDinamica.isEmpty());
+		assertTrue(dynamicQueue.isEmpty());
 	}
 	
 	@Test
-	void emptyRemoveDouble() {
-		filaDinamica.insert("A");
-		filaDinamica.remove();
-		filaDinamica.remove();
-		
-		assertTrue(filaDinamica.isEmpty());
+	@DisplayName("Remove index without element in throw exception")
+	@Order(value = 3)
+	void removeIndexOut() {
+		assertThrows(IndexOutOfBoundsException.class, () -> dynamicQueue.remove());
 	}
 	
 	@Test
-	void addValueFixed(Object e) {
-		final String valor = "A";
-		filaDinamica.insert(valor);
-		
-		assertEquals(valor, filaDinamica.remove());
-	}
-	
-	@Test
-	void addNull(Object e) {
-		filaDinamica.insert(null);
-		assertNull(filaDinamica.remove());
-	}
-	
-	@Test
-	void removeIndexOff() {
-		assertThrows(IndexOutOfBoundsException.class, () -> filaDinamica.remove());
-	}
-	
-	@Test
+	@DisplayName("Queue size 0")
+	@Order(value = 4)
 	void sizeZero() {
-		assertEquals(0, filaDinamica.size());
+		assertEquals(0, dynamicQueue.size());
 	}
 }
