edd28c883cd8ec0c39347a8711931e151193c8d8
= <ryaninternet62@gmail.com>
2022-12-12 06:54:59 -0300

    Adicionei as aplicações da atividade de recursividade.

--- src/main/java/br/com/ryan/recursividade/Recursividade.java
+++ src/main/java/br/com/ryan/recursividade/Recursividade.java
@@ -2,4 +2,57 @@ package br.com.ryan.recursividade;
 
 public class Recursividade {
 	
+	public double fatorialSobre(long valor) {
+		if (valor <= 0) {
+			return 0;
+		}
+		
+		double resultado = (double) 1 / fatorial(valor);
+		return resultado + fatorialSobre(--valor);
+	}
+	
+	public long fatorial(long valor) throws IllegalArgumentException {
+		if (valor < 0) {
+			throw new IllegalArgumentException("Não existe fatorial de número negativo.");
+		}
+		
+		if (valor < 2) {
+			return 1;
+		}
+		
+		return valor * fatorial(valor - 1);
+	}
+	
+	public int fibonacci(int parametro) {
+		if (parametro < 0) {
+			throw new IllegalArgumentException("Os números negativos em fibonacci são absurdos.");
+		}
+		
+		if (parametro == 0) {
+			return 0;
+		}
+		
+		if (parametro == 1 || parametro == 2) {
+			return 1;
+		}
+		
+		return fibonacci(parametro - 1) + fibonacci(parametro - 2);
+	}
+	
+	public int somarPositivo(int valor) {
+		if (valor < 1) {
+			return 0;
+		}
+		
+		return ((valor) + somarPositivo(valor - 1));
+	}
+	
+	public double somarSobrePositivo(int valor) {
+		if (valor <= 0) {
+			return 0;
+		}
+		
+		double resultado = (double) 1 / valor;
+		return resultado + somarSobrePositivo(--valor);
+	} 
 }
---
+++ src/test/java/br/com/ryan/recursividade/RecursividadeTeste.java
@@ -0,0 +1,113 @@
+package br.com.ryan.recursividade;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@DisplayName("Recursividade")
+class RecursividadeTeste {
+	
+	private Recursividade recursividade;
+	
+	@BeforeEach
+	void novo() {
+		recursividade = new Recursividade();
+	}
+	
+	@Test
+	@DisplayName("Fatorial sobre 1 passando zero.")
+	void fatorialSobreZerado() {
+		assertEquals(0.0, recursividade.fatorialSobre(0));
+	}
+	
+	@Test
+	@DisplayName("Fatorial sobre 1 passando negativo.")
+	void fatorialSobreNegativo() {
+		assertEquals(0.0, recursividade.fatorialSobre(-1));
+	}
+	
+	@Test
+	@DisplayName("Fatorial sobre 1 passando positivo.")
+	void fatorialSobrePositivo() {
+		assertEquals(1.0, recursividade.fatorialSobre(1));
+	}
+	
+	@Test
+	@DisplayName("Fatorial zerado.")
+	void fatorialZerado() {
+		assertEquals(1, recursividade.fatorial(0));
+	}
+	
+	@Test
+	@DisplayName("Fatorial negativo.")
+	void fatorialNegativo() {
+		assertThrows(IllegalArgumentException.class, () -> recursividade.fatorial(-1));
+	}
+	
+	@Test
+	@DisplayName("Fatorial positivo.")
+	void fatorialPositivo() {
+		assertEquals(1, recursividade.fatorial(1));
+	}
+	
+	@Test
+	@DisplayName("Fibonacci zerado.")
+	void fibonacciZerado() {
+		assertEquals(0, recursividade.fibonacci(0));
+	}
+	
+	@Test
+	@DisplayName("Fibonacci negativo.")
+	void fibonacciNegativo() {
+		assertThrows(IllegalArgumentException.class, () -> recursividade.fibonacci(-1));
+	}
+	
+	@Test
+	@DisplayName("Fibonacci positivo.")
+	void fibonacciPositivo() {
+		assertEquals(1, recursividade.fibonacci(1));
+		assertEquals(1, recursividade.fibonacci(2));
+	}
+	
+	@Test
+	@DisplayName("Somar positivo passando zero.")
+	void somarPositivoComZero() {
+		assertEquals(0, recursividade.somarPositivo(0));
+	}
+	
+	@Test
+	@DisplayName("Somar positivo passando negativo.")
+	void somarPositivoComNegativo() {
+		assertEquals(0, recursividade.somarPositivo(-1));
+	}
+	
+	@Test
+	@DisplayName("Somar positivo passando positivo.")
+	void somarPositivoComPositivo() {
+		assertEquals(55, recursividade.somarPositivo(10));
+	}
+	
+	@Test
+	@DisplayName("Somar sobre zerado.")
+	void somarSobreZerado() {
+		assertEquals(0, recursividade.somarSobrePositivo(0));
+	}
+	
+	@Test
+	@DisplayName("Somar sobre negativo.")
+	void somarSobreNegativo() {
+		assertEquals(0, recursividade.somarSobrePositivo(-1));
+	}
+	
+	@Test
+	@DisplayName("Somar sobre positivo.")
+	void somarSobrePositivo() {
+		assertEquals(1, recursividade.somarSobrePositivo(1));
+	}
+}
